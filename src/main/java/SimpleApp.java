d01389c792ff940ebc252ffcd21e5eb7a42a136a
wojtala6 <wojtala6@gmail.com>
2015-04-19 17:38:20 +0200

    Added possibility to use MSGFPlus

--- src/main/java/SimpleApp.java
+++ src/main/java/SimpleApp.java
@@ -5,23 +5,40 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class SimpleApp {
 	public static void main(String[] args) {
 		//System.setProperty("hadoop.home.dir", "d:\\winutil\\");
-		if(args.length < 1) {
-			System.out.println("Nie przekazano parametru");
+		
+		if(args.length < 2) {
+			System.out.println("Too few arguments");
+			System.exit(1);
+		} else if (args.length > 2) {
+			System.out.println("Too many arguments");
 			System.exit(1);
 		}
-		final String pathToInputFiles = args[0]; //"/user/root/pepnovo3/";
+		final String programToUse = args[0];
+		String bashScriptLocation = "";
+		if(!programToUse.equals("pepnovo3") && !programToUse.equals("msgf")) {
+			System.out.println("Bad program to use");
+			System.exit(1);
+		} else if (programToUse.equals("pepnovo3")) {
+			bashScriptLocation = "/home/ec2-user/read.sh";
+		} else {
+			bashScriptLocation = "/home/ec2-user/msgf/msgf.sh";
+		}
+		
+		final String pathToInputFiles = args[1];
 		
 		SparkConf conf = new SparkConf().setAppName("Simple Application");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
-		//String pepnovoFile = folderPath + "biginputfile.mgf";
-		//FileDivider.divideFile(pepnovoFile, "1MB", false);
+		/*String pepnovoFile = "/home/ec2-user/test/smallinputfile.mgf";
+		FileDivider.divideFile(pepnovoFile, "256KB", false);*/
 		JavaPairRDD<String,String> pepnovoFiles = sc.wholeTextFiles(pathToInputFiles, 4).cache();
 		
 		long startTime = System.currentTimeMillis();
-		pepnovoFiles.pipe("/home/ec2-user/read.sh").collect();
+		pepnovoFiles.pipe(bashScriptLocation).collect();
 		System.out.println("Computations takes " + (System.currentTimeMillis()- startTime) + "ms");
-		pepnovoFiles.saveAsTextFile(pathToInputFiles + "testoutput1.txt"); //coalesce(1,true) repartition(1)
+		pepnovoFiles.saveAsTextFile(pathToInputFiles + "testoutput.tsv");
+		
+		//pepnovoFiles.saveAsTextFile(pathToInputFiles + "testoutput1.txt"); //coalesce(1,true) repartition(1)
 		
 		sc.close();
 
